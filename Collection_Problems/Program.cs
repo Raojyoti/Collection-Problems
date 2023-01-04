5f83b51e6503ae994cce263ee59520e730d8f1d5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-04 18:24:20 +0530

    [Jyoti Rao]List: Add values in List

---
+++ Collection_Problems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection_Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List();
+        }
+        public static void List()
+        {
+            List<int> list = new List<int>();
+            list.Add(50);
+            list.Add(40);
+            list.Add(20);
+            list.Add(70);
+            list.Add(90);
+            Console.WriteLine("Values in List are: ");
+            Console.WriteLine("-----------------------------------------------");
+            foreach (var values in list)
+            {
+                Console.WriteLine(values);
+            }
+        }
+    }
+}
